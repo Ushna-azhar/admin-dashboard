c11ccc3e419f29cadd171a324d5ae55ed5db5590
Ushna-azhar <773.ushnaazhar@gmail.com>
2025-01-24 14:44:42 +0500

    Update Chart.tsx

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,10 +1,15 @@
 // src/components/chart.tsx
 import { Bar } from "react-chartjs-2";
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+import { ChartData } from "chart.js"; // Import the ChartData type
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-const Chart = ({ data }: { data: any }) => {
+interface ChartProps {
+  data: ChartData<'bar'>; // Specify the data type for a Bar chart
+}
+
+const Chart = ({ data }: ChartProps) => {
   return (
     <div className="mb-8">
       <Bar
